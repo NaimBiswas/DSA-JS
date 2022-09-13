959e3bf0bbbb33473b5726d4d1865164684b1e2e
NaimBiswas <nayeembiswas2@gmail.com>
2022-09-13 22:40:50 +0600

    Binary Search Tree

--- index.js
+++ index.js
@@ -58,10 +58,62 @@ class linkedList {
   }
 }
 
-const myLinkedList = new linkedList(3);
-myLinkedList.append(5);
-myLinkedList.append(10);
-myLinkedList.append(25);
-myLinkedList.preAppend(1);
-myLinkedList.preAppend(2);
-console.log(JSON.stringify(myLinkedList.head), myLinkedList.length);
+// const myLinkedList = new linkedList(3);
+// myLinkedList.append(5);
+// myLinkedList.append(10);
+// myLinkedList.append(25);
+// myLinkedList.preAppend(1);
+// myLinkedList.preAppend(2);
+// console.log(JSON.stringify(myLinkedList.head), myLinkedList.length);
+
+// Binary Search with linkedList
+class Node {
+  constructor(value) {
+    (this.left = null), (this.right = null);
+    this.value = value;
+  }
+}
+
+class BinarySearchTree {
+  constructor(props) {
+    this.root = null;
+  }
+  insert(value) {
+    const newValue = new Node(value);
+    if (!this.root) {
+      this.root = newValue;
+    }
+    let currentNode = this.root;
+    while (true) {
+      if (value < currentNode.value) {
+        // left
+        if (!currentNode.left) {
+          currentNode.left = newValue;
+          console.log(currentNode);
+          return this;
+        }
+        currentNode = currentNode.left;
+      } else {
+        // right
+        if (!currentNode.right) {
+          currentNode.right = newValue;
+          console.log(currentNode);
+          return this;
+        }
+        currentNode = currentNode.right;
+      }
+    }
+    return this;
+  }
+  lookup(value) {}
+  remove(value) {}
+}
+
+// const Tree = new BinarySearchTree();
+// Tree.insert(20);
+// Tree.insert(30);
+// Tree.insert(35);
+// console.log(Tree);
+//      20
+//    15   30
+//  13        35
